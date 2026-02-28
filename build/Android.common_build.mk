bdf6f8941401685df11f6b1629c503b9868ae8bf
Lani <puntillol@hotmail.com>
2026-02-27 21:37:35 -0500

    Fix valgrind for ART

--- build/Android.common_build.mk
+++ build/Android.common_build.mk
@@ -189,8 +189,8 @@ ART_CPP_EXTENSION := .cc
 
 ART_C_INCLUDES := \
   external/gtest/include \
-  external/valgrind/main/include \
-  external/valgrind/main \
+  external/valgrind/include \
+  external/valgrind/ \
   external/vixl/src \
   external/zlib \
 
